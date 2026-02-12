57e6dcc8d970606a7ddf7e9afb448444db08e829
Muhammad Akbar <hibarxnl@gmail.com>
2026-02-12 21:37:02 +0800

    Update tools-getid.js
    Reply

--- plugins/tools-getid.js
+++ plugins/tools-getid.js
@@ -6,18 +6,34 @@ module.exports = {
 
   async handler({ m }) {
     const key = m.key || {}
+    const ctx = m.message?.extendedTextMessage?.contextInfo || {}
 
-    const lid =
-      key.participant ||
-      m.sender ||
-      null
+    let lid = null
+    let wa = null
 
-    const wa =
-      key.participantAlt ||
-      (lid ? lid.replace("@lid", "@s.whatsapp.net") : null)
+    
+    if (ctx.participant) {
+      lid = ctx.participant
+      wa =
+        ctx.participantAlt ||
+        (lid ? lid.replace("@lid", "@s.whatsapp.net") : null)
+    }
+
+    
+    else if (ctx.mentionedJid?.length) {
+      lid = ctx.mentionedJid[0]
+      wa = lid.replace("@lid", "@s.whatsapp.net")
+    }
+
+    
+    else {
+      lid = key.participant || m.sender
+      wa =
+        key.participantAlt ||
+        (lid ? lid.replace("@lid", "@s.whatsapp.net") : null)
+    }
 
     let text = "🆔 *USER ID INFO*\n\n"
-
     text += `🔹 LID ID : ${lid || "-"}\n`
     text += `🔹 WA ID  : ${wa || "-"}\n`
     text += `🔹 CHAT   : ${m.chat}\n`
